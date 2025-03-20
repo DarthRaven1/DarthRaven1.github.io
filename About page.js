f3e66f3a48404de2eb53c561bcac45667b7bdf02
DarthRaven1 <152425782+DarthRaven1@users.noreply.github.com>
2025-03-20 13:15:49 -0600

    Add files via upload

---
+++ About page.js
@@ -0,0 +1,9 @@
+const myMap = new Map();
+
+myMap.set('name', 'Asher Lilly');
+
+console.log(myMap.get('name')); //getting the value of the 'name' key
+
+console.log(myMap.has('name')); //checking if the 'name' key exists in the map
+
+console.log(myMap.size); //checking the size of the map       
